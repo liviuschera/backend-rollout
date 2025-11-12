f19ea9410b2d118c7c74c78ff53002b2f502576c
Liviu Schera <liviu.schera@gmail.com>
2025-11-12 11:39:40 +0200

    Update application to return hostname and configure deployment settings

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 """Example of flask main file."""
+import os
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/api/hello')
 def hello_world():
-    """Returns Hello, EDP!"""
-    return 'Hello, EDP!'
+    """Returns Hello, KubeRocketCI from hostname."""
+    hostname = os.getenv('HOSTNAME', 'unknown')
+    return f'Hello, KubeRocketCI from {hostname}'
 
 
 if __name__ == '__main__':
